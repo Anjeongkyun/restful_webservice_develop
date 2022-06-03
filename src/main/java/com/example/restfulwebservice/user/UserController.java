f7535b81be82281aec777fee756d77e08852ee62
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-03 17:36:22 +0900

    User Controller 생성

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,2 +1,30 @@
-package com.example.restfulwebservice.user;public class UserController {
+package com.example.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserDaoService userDaoService;
+
+    public UserController(UserDaoService userDaoService){
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public User retrieveUsers(@PathVariable int id) {
+        return userDaoService.findOne(id);
+    }
+
+    @PostMapping("users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+    }
 }
