5d618efbc9fef8da8b7cb64b1020cc5db961e092
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-04 13:06:17 +0900

    ServletUriComponentsBuilder를 사용하여 올바른 status code와 Return uri를 header에 담기

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,13 +21,20 @@ public class UserController {
         return userDaoService.findAll();
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/users/{id}")
     public User retrieveUsers(@PathVariable int id) {
         return userDaoService.findOne(id);
     }
 
     @PostMapping("users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
