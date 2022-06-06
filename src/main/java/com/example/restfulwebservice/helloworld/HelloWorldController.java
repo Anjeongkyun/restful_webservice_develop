d35511c3269c9505c14db04e533f7eb25714c806
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-06 23:16:20 +0900

    다국어 전환 소스 추가

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.example.restfulwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSoruce;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -21,6 +29,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
 
         return new HelloWorldBean(String.format("name : %s" , name));
+    }
+
+    @GetMapping(path = "/hello-world-internaionalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+        return messageSoruce.getMessage("greeting.message",null, locale);
     }
 
 }
