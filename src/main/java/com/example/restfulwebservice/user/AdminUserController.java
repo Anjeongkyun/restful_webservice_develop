0104bd4ed1b5faeaeed6d25c76455671f76e8db5
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-07 20:27:58 +0900

    AdminUserController 생성

---
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -0,0 +1,36 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+
+    private UserDaoService userDaoService;
+
+    public AdminUserController(UserDaoService userDaoService){
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUsers(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
+    }
+}
