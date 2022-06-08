2a919ad47aa756392a51322c384223b8810693df
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-08 11:03:06 +0900

    jpa 특정 사용자 조회

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -2,10 +2,12 @@ package com.example.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
@@ -18,5 +20,16 @@ public class UserJpaController {
     public List<Users> retrieveAllUsers() {
 
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Users retrieveUsers(@PathVariable int id) {
+
+        Optional<Users> user = userRepository.findById(id);
+        if(!user.isPresent()){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user.get();
     }
 }
