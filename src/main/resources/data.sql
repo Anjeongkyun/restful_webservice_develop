6ad4a00e90ff6e2cacf842b851429335852827e7
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-08 10:54:31 +0900

    jpa를 이용한 사용자 전체 조회 기능 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users values(1, now(), 'User1', 'test1111','961111-1111111');
+insert into users values(2, now(), 'User2', 'test1111','962222-2222222');
+insert into users values(3, now(), 'User3', 'test1111','963333-3333333');
